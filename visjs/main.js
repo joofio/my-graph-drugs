71c254d14ad7f65156237c61a8169e64bc7ee165
João Almeida <joaofilipe90@gmail.com>
2021-08-30 13:40:41 +0100

    dynamic filtering

--- visjs/main.js
+++ visjs/main.js
@@ -17,10 +17,12 @@ const options = {
   },
 };
 
+
 const nodes = [
   {
     id: "MedicationKnowledge/25",
     size: 100,
+    productType: "Substance",
     label: "Paracetamol:\nStatus: Active\nProductType: Substance\n Synonym: AcetominoPhen",
     color: "#E5E5FF",
     shape: "box",
@@ -29,6 +31,7 @@ const nodes = [
   {
     id: "MedicationKnowledge/24",
     size: 100,
+    productType: "Substance",
     label: "Diclofenac:\nStatus: Active\nProductType: Substance",
     color: "#E5E5FF",
     shape: "box",
@@ -37,6 +40,7 @@ const nodes = [
   {
     id: "MedicationKnowledge/27",
     size: 120,
+    productType: "Pharmaceutical Product",
     label:"Diclofenac 50 mg Comprimido revestido\nRoute: Oral\n productType: Pharmaceutical Product\nDoseFrom: Comprimido Revestido",
     color: "#FFCFCF",
     shape: "box",
@@ -47,21 +51,25 @@ const nodes = [
 label: "ProductTye: medproduct\n  status: active\n synonym: Diclofenac Sandoz 50 mg Comprimidos revestidos 50 mg Comprimido revestido\n productType: MEDPROD\n productType_0_coding_0_system: http://terminology.hl7.org/CodeSystem/medicineCS\n amount: 50\n amount_unit: mg\n doseForm: 38.0\n doseForm_coding_0_system: http://infarmed.pt/pharm_Form\n doseForm_coding_0_display: Comprimido revestido\n ",
 color: "#DAF7A6",
 shape: "box",
+productType: "Medicinal Product",
 font: { face: "monospace", align: "left" }},
 {id: "MedicationKnowledge/35",
 size: 150,
-label: "ProductTye: medproduct\n  status: active\n synonym: Cataflam 50 mg Comprimido revestido\n productType: MEDPROD\n productType_0_coding_0_system: http://terminology.hl7.org/CodeSystem/medicineCS\n amount: 50\n amount_unit: mg\n doseForm: 38.0\n doseForm_coding_0_system: http://infarmed.pt/pharm_Form\n doseForm_coding_0_display: Comprimido revestido\n ",
+label: "ProductTye: medproduct\nstatus: active\nsynonym: Cataflam 50 mg Comprimido revestido\n productType: MEDPROD\n productType_0_coding_0_system: http://terminology.hl7.org/CodeSystem/medicineCS\n amount: 50\n amount_unit: mg\n doseForm: 38.0\n doseForm_coding_0_system: http://infarmed.pt/pharm_Form\n doseForm_coding_0_display: Comprimido revestido\n ",
 color: "#DAF7A6",
 shape: "box",
+productType: "Medicinal Product",
 font: { face: "monospace", align: "left" }},
 {id: "MedicationKnowledge/36",
 size: 200,
+productType: "Packaged Medicinal Product",
 label: "ProductTye: pack_med_product\n  status: active\n synonym: Cataflam 50 mg Comprimido revestido Blister - 10 unidade(s)\n productType: PACKMEDPROD\n productType_0_coding_0_system: http://terminology.hl7.org/CodeSystem/medicineCS\n amount: 50\n amount_unit: mg\n doseForm: 38.0\n doseForm_coding_0_system: http://infarmed.pt/pharm_Form\n doseForm_coding_0_display: Comprimido revestido\n ",
 color: "#76D7C4",
 shape: "box",
 font: { face: "monospace", align: "left" }},
 {id: "MedicationKnowledge/37",
 size: 200,
+productType: "Packaged Medicinal Product",
 label: "ProductTye: pack_med_product\n  status: active\n synonym: Cataflam 50 mg Comprimido revestido Blister - 30 unidade(s)\n productType: PACKMEDPROD\n productType_0_coding_0_system: http://terminology.hl7.org/CodeSystem/medicineCS\n amount: 50\n amount_unit: mg\n doseForm: 38.0\n doseForm_coding_0_system: http://infarmed.pt/pharm_Form\n doseForm_coding_0_display: Comprimido revestido\n ",
 color: "#76D7C4",
 shape: "box",
