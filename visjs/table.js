91f5283136014a44f344ef7fabc78a642f6e15c6
João Almeida <joaofilipe90@gmail.com>
2021-09-03 12:02:03 +0100

    first try

---
+++ visjs/table.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+  var table = $('#example').DataTable({
+   // lengthMenu: [[20, 50, 100, -1], [20, 50, 100, "All"]],
+    pageLength: 25,
+    orderCellsTop: true,
+    fixedHeader: true,
+    data: nodes,
+    "columns": [
+      { data: "id" },
+      { data: "productType",
+      "defaultContent": "<i>Not Applicable</i>" },
+      { data: "doseForm",
+      "defaultContent": "<i>Not Applicable</i>" },
+      { data: "route",
+      "defaultContent": "<i>Not Applicable</i>" },
+      { data: "label" },
+      { data: "Graph",
+             "defaultContent": "<i>Not Applicable</i>" }
+
+    ],
+    initComplete: function () {
+      count = 0;
+      this.api().columns().every(function () {
+        var title = this.header();
+        //replace spaces with dashes
+        title = $(title).html().replace(/[\W]/g, '-');
+        var column = this;
+        var select = $('<select id="' + title + '" class="select2" ></select>')
+          .appendTo($(column.footer()).empty())
+          .on('change', function () {
+            //Get the "text" property from each selected data 
+            //regex escape the value and store in array
+            var data = $.map($(this).select2('data'), function (value, key) {
+              return value.text ? '^' + $.fn.dataTable.util.escapeRegex(value.text) + '$' : null;
+            });
+
+            //if no data selected use ""
+            if (data.length === 0) {
+              data = [""];
+            }
+
+            //join array into string with regex or (|)
+            var val = data.join('|');
+
+            //search for the option(s) selected
+            column
+              .search(val ? val : '', true, false)
+              .draw();
+          });
+
+        column.data().unique().sort().each(function (d, j) {
+          select.append('<option value="' + d + '">' + d + '</option>');
+        });
+
+        //use column title as selector and placeholder
+        $('#' + title).select2({
+          multiple: true,
+          closeOnSelect: false,
+          placeholder: "Select " + title
+        });
+
+        //initially clear select otherwise first option is selected
+        $('.select2').val(null).trigger('change');
+      });
+    }
+  });
+
+});
