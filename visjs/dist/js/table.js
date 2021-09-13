86ca3bb5b563e20128ed1905aa1133c4de877041
João Almeida <joaofilipe90@gmail.com>
2021-09-13 19:34:40 +0100

    strength

--- visjs/dist/js/table.js
+++ visjs/dist/js/table.js
@@ -1,36 +1,58 @@
 $(document).ready(function () {
- // $('#example thead tr').clone(true).addClass('filters').appendTo( '#example thead' );
+  // $('#example thead tr').clone(true).addClass('filters').appendTo( '#example thead' );
 
   var table = $('#example').DataTable({
-   // lengthMenu: [[20, 50, 100, -1], [20, 50, 100, "All"]],
+    // lengthMenu: [[20, 50, 100, -1], [20, 50, 100, "All"]],
     pageLength: 5,
     orderCellsTop: true,
     fixedHeader: true,
-  //  search: {
+    //  search: {
     //    return: true //for only processing when key is pressed - usefull for lots of data
-   // },
-   // data: nodes,
-   "ajax": "data/data.json",
+    // },
+    // data: nodes,
+    "ajax": "data/data.json",
     "columns": [
-      { data: "id"},
-      {data: "CNPEM",
-      "defaultContent": "<i>Not Available</i>"},
-      { data: "name"},
+      { data: "id" },
 
-     
-      { data: "doseForm",
-      "defaultContent": "<i>Not Applicable</i>",render:function ( data ) {
-        return '<a href="'+data+'">'+data+'</a>';} },//links
-        { data: "strength",
-        "defaultContent": "<i>Not Applicable</i>" },
-      { data: "route",
-      "defaultContent": "<i>Not Applicable</i>" },
-      { data: "productType",
-      "defaultContent": "<i>Not Applicable</i>" },
-      { data: "id",render:function ( data ) {
-        return '<a class="btn btn-primary" href="index.html?substance='+data+'" role="button">See Graph</a>';} } 
+      {
+        data: "CNPEM",
+        "defaultContent": "<i>Not Available</i>"
+      },
 
-    ], 
+      { data: "name" },
+
+      {
+        data: "doseForm",
+        "defaultContent": "<i>Not Applicable</i>", render: function (data) {
+          if (data === undefined) {
+            return '<a><i>Not Applicable</i></a>';
+          }
+          return '<a href="' + data + '">' + data + '</a>';
+        }
+      },//links
+
+      {
+        data: "strength",
+        "defaultContent": "<i>Not Applicable</i>"
+      },
+
+      {
+        data: "route",
+        "defaultContent": "<i>Not Applicable</i>"
+      },
+
+      {
+        data: "productType",
+        "defaultContent": "<i>Not Applicable</i>"
+      },
+      
+      {
+        data: "id", render: function (data) {
+          return '<a class="btn btn-primary" href="index.html?substance=' + data + '" role="button">See Graph</a>';
+        }
+      }
+
+    ],
 
     initComplete: function () {
       count = 0;
